a8462c21b49626eababb21f7bbdfa32f5579eeac
Javier Ramon Avila <javi0098@gmail.com>
2021-09-21 18:56:54 -0300

    agregacion del Formulario Git

---
+++ SI.GE.TA.M/Frm_Prueba_Git.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SI.GE.TA.M
+{
+    public partial class Frm_Prueba_Git : Form
+    {
+        public Frm_Prueba_Git()
+        {
+            InitializeComponent();
+        }
+
+        private void Frm_Prueba_Git_Load(object sender, EventArgs e)
+        {
+            // aca iria la logica detras
+        }
+    }
+}
